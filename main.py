9922549d94dd23763eec67f8680ed6798f3f576d
hankertrix <91734413+hankertrix@users.noreply.github.com>
2025-03-16 00:43:06 +0800

    Add data files and the Jupyter percent format script

---
+++ main.py
@@ -0,0 +1,26 @@
+# The Python file in the percent Jupyter notebook format.
+#
+# Format specification:
+# https://jupytext.readthedocs.io/en/latest/formats-scripts.html
+
+# %% [markdown]
+# # MA2018 Mini Project
+# This project makes use of the climate change dataset.
+
+# %% [markdown]
+# Import all the required libraries.
+
+# %%
+import pandas as pd
+
+# %% [markdown]
+# The constants used in the program.
+
+# %%
+DATA_FILE = "./data.xls"
+
+# %% [markdown]
+# Read the data from the data file.
+
+# %%
+data = pd.read_excel(DATA_FILE)

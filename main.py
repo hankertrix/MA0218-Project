d0d017654b6807d9a358fb4c26068638437337b4
hankertrix <91734413+hankertrix@users.noreply.github.com>
2025-03-21 04:45:47 +0800

    Migrate to use Marimo instead of Jupyter notebooks

--- main.py
+++ main.py
@@ -1,33 +1,63 @@
-# The Python file in the percent Jupyter notebook format.
-#
-# Format specification:
-# https://jupytext.readthedocs.io/en/latest/formats-scripts.html
+import marimo
 
-# %% [markdown]
-# # MA2018 Mini Project
-# This project makes use of the climate change dataset.
+__generated_with = "0.11.22"
+app = marimo.App()
 
-# %% [markdown]
-# Import all the required libraries.
 
-# %%
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sb
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+        # MA2018 Mini Project
+        This project makes use of the climate change dataset.
+        """
+    )
+    return
 
-# Set the seaborn style
-sb.set_theme()
 
-# %% [markdown]
-# The constants used in the program.
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""Import all the required libraries.""")
+    return
 
-# %%
-DATA_FILE = "./data.xls"
 
-# %% [markdown]
-# Read the data from the data file.
+@app.cell
+def _():
+    import marimo as mo
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import pandas as pd
+    import seaborn as sb
 
-# %%
-data = pd.read_excel(DATA_FILE)
-print(data.head())
+    # Set the seaborn style
+    sb.set_theme()
+    return mo, np, pd, plt, sb
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""The constants used in the program.""")
+    return
+
+
+@app.cell
+def _():
+    DATA_FILE = "./data.xls"
+    return (DATA_FILE,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""Read the data from the data file.""")
+    return
+
+
+@app.cell
+def _(DATA_FILE, pd):
+    data = pd.read_excel(DATA_FILE)
+    print(data.head())
+    return (data,)
+
+
+if __name__ == "__main__":
+    app.run()
